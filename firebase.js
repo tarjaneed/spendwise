6a56b43c58d13a846861106b91229e28258e0f1c
tarjaneed <tarjaneed@gmail.com>
2023-11-22 15:30:38 -0500

    CRUD Operations

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDMpcCuyl3mk0IJo7sr93T3BKtMDGP8GHk",
+    authDomain: "spendwise-6b61a.firebaseapp.com",
+    projectId: "spendwise-6b61a",
+    storageBucket: "spendwise-6b61a.appspot.com",
+    messagingSenderId: "754157392353",
+    appId: "1:754157392353:web:e597e6e42540cd4e5082a0",
+    measurementId: "G-LECW3ZV5EZ"
 };
 
 // Initialize Firebase
@@ -18,4 +19,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Firestore DB
+export const db = getFirestore(app);
+
 export default app;
--- screens/add-category/Styles.js
+++ screens/add-category/Styles.js
@@ -4,50 +4,55 @@ let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-   header: {
-      flexDirection: 'row',
-      marginTop: 10,
-      paddingHorizontal: 10,
-     justifyContent: 'space-between',
+    loader: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        flexDirection: 'row',
+        marginTop: 10,
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
     },
     input: {
-      borderBottomWidth: 1,
-      width: deviceWidth / 1.4,
-      borderBottomColor: '#D3D3D3',
-      fontSize: 17,
+        borderBottomWidth: 1,
+        width: deviceWidth / 1.4,
+        borderBottomColor: '#D3D3D3',
+        fontSize: 17,
     },
     card: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: '#fff',
-      borderRadius: 10,
-      marginTop: 15,
-      marginHorizontal: 10,
-      height: deviceHeight/20,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginTop: 15,
+        marginHorizontal: 10,
+        height: deviceHeight / 20,
     },
     color: {
-      marginRight: 10,
-      width: 15,
-      height: 15,
-      borderRadius: 15 / 2,
+        marginRight: 10,
+        width: 15,
+        height: 15,
+        borderRadius: 15 / 2,
     },
     iconsContainer: {
-      flex: 1,
-      alignItems: 'center',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
     },
     addButton: {
-      borderRadius: 10,
-      marginHorizontal: 4,
-      marginVertical: 4,
-      justifyContent: 'center',
-      padding: 3,
-      backgroundColor: '#23c0f2',
-      fontWeight: 'bold'
-  },
-    
-  });
+        borderRadius: 10,
+        marginHorizontal: 4,
+        marginVertical: 4,
+        justifyContent: 'center',
+        padding: 3,
+        backgroundColor: '#23c0f2',
+        fontWeight: 'bold'
+    },
+
+});
 
 export { styles };
 
