2d2fdb09226b2b62a50b84dfe54739b0b733abdd
tarjaneed <tarjaneed@gmail.com>
2023-12-02 10:07:19 -0500

    Code formatting

--- components/pieChart/PieChart.js
+++ components/pieChart/PieChart.js
@@ -1,26 +1,26 @@
-import { View , Text} from 'react-native';
+import { View, Text } from 'react-native';
 import PieChart from 'react-native-expo-pie-chart';
 import { styles } from './Styles';
 
-const App = ({ categories , total}) => {
+const App = ({ categories, total }) => {
 
-  const chartData = categories.map((item) => ({
-    key: item.name,
-    count: item.total,
-    color: item.color,
-  }));
+    const chartData = categories.map((item) => ({
+        key: item.name,
+        count: item.total,
+        color: item.color,
+    }));
 
-  return (
-    <View style={styles.container}>
-      <PieChart
-        data={chartData}
-        length={220}
-      />
-        <View style={styles.gauge}>
-        <Text style ={styles.gaugeText}>${total}</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <PieChart
+                data={chartData}
+                length={220}
+            />
+            <View style={styles.gauge}>
+                <Text style={styles.gaugeText}>${total}</Text>
+            </View>
+        </View>
+    );
 };
 
 export default App;
--- components/pieChart/Styles.js
+++ components/pieChart/Styles.js
@@ -1,25 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  gauge: {
-    position: 'absolute',
-    width: 100,
-    height: 180,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  gaugeText: {
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    fontSize: 25, 
-    color: '#005A9C',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    gauge: {
+        position: 'absolute',
+        width: 100,
+        height: 180,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    gaugeText: {
+        backgroundColor: 'transparent',
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: '#005A9C',
+    },
 });
 
-  export { styles };
+export { styles };
