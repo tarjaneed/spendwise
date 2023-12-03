04782a1e9c558485e59551f94df6cfa5803baf71
tarjaneed <tarjaneed@gmail.com>
2023-12-03 17:29:52 -0500

    Error handling while call and email opening

--- screens/forgotuserpass/ForgotUserPassScreen.js
+++ screens/forgotuserpass/ForgotUserPassScreen.js
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
-import call from 'react-native-phone-call';
-import { Text, View, Image, TouchableOpacity, Linking } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Linking, Alert, Platform } from 'react-native';
 import { styles } from './Styles';
-
-
 
 const ForgotUserPassScreen = ({ navigation }) => {
 
     const [inputValue, setInputValue] = useState('1-800-773-6399');
 
-    const triggerCall = () => {
-        const args ={
-            number: inputValue,
-            prompt: true,
-        };
-        // Make a call
-        call(args).catch(console.error);
+    const openURL = async (url) => {
+        const supported = await Linking.canOpenURL(url);
+        if (supported) {
+            await Linking.openURL(url);
+        } else {
+            Alert.alert('No app to open the URL');
+        }
     }
-    
+
     return (
         <View style={styles.container}>
             <View style={styles.headingContainer}>
@@ -32,57 +29,57 @@ const ForgotUserPassScreen = ({ navigation }) => {
             </View>
             <View style={styles.infoContainer}>
                 <View style={styles.contactContainer}>
-                    <Image 
-                        source={{uri: 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png'}}
+                    <Image
+                        source={{ uri: 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png' }}
                         style={styles.icon}
                     />
                     <View style={styles.textbox}>
                         <TouchableOpacity
                             activeOpacity={0.7}
-                            onPress={triggerCall}>
+                            onPress={() => {
+                                openURL( Platform.OS == 'android' ? `tel:${inputValue}` : `telprompt:${inputValue}`)}
+                            }>
                             <Text style={styles.textboxText}>
                                 1-800-SPENDWZ   (1-800-773-6399)
                             </Text>
                         </TouchableOpacity>
                     </View>
-				</View>
+                </View>
                 <View style={styles.contactContainer}>
-                    <Image 
-                        source={{uri: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-email-512.png'}}
+                    <Image
+                        source={{ uri: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-email-512.png' }}
                         style={styles.icon}
                     />
                     <View style={styles.textbox}>
-                        <TouchableOpacity  
+                        <TouchableOpacity
                             activeOpacity={0.7}
-                            onPress={()=>{
-                                Linking.openURL('mailto:support@spendwise.com')
-                            }}>
+                            onPress={() => openURL('mailto:support@spendwise.com')}>
                             <Text style={styles.textboxText}>
                                 support@spendwise.com
                             </Text>
                         </TouchableOpacity>
                     </View>
-				</View>
+                </View>
             </View>
 
 
 
-            
+
             <View style={styles.returntoLoginContainer}>
                 <TouchableOpacity
-                        onPress={() => {
-                            navigation.navigate('SignIn')
-                        }}
-                    >
-                        <Text style={styles.returntoLoginText}>
-                            Return to Login
-                        </Text>
+                    onPress={() => {
+                        navigation.navigate('SignIn')
+                    }}
+                >
+                    <Text style={styles.returntoLoginText}>
+                        Return to Login
+                    </Text>
                 </TouchableOpacity>
             </View>
 
         </View>
 
-    ); 
+    );
 };
 
 export default ForgotUserPassScreen;
